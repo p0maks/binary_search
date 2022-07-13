2a3f83582bc0be0d5d6b46122512fc9dff7eb3b7
p0maks <golosova_v19@mail.ru>
2022-07-13 19:57:17 +0300

    one_commit

--- binary/binary.cpp
+++ binary/binary.cpp
@@ -2,9 +2,29 @@
 //
 
 #include <iostream>
+using namespace std;
+const int N = 10;
 
-int main()
+int BinarySearch(int A[], int key)
 {
-    std::cout << "Hello World!\n";
+	int left = 0, right = N, mid;
+	while (left <= right)
+	{
+		mid = left + (right - left) / 2;
+		if (key < A[mid]) 
+			right = mid - 1;
+		else if (key > A[mid]) 
+			left = mid + 1;
+		else 
+			return mid;
+	}
+	return -1;
+}
+
+void main()
+{
+	setlocale(LC_ALL, "Rus");
+	
+	system("pause");
 }
 
